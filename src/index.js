ba336ffe92926537087912b92e0fbc83600b1b26
Bhekumuzi Tshabalala <bhekumuzitshabalala.main@gmail.com>
2023-12-22 11:34:31 +0200

    nothing major

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App(){
+    return (<container>
+        <div>
+            <h1>Starting Page</h1>
+            <p>Thenga is a platform that connects local merchants to customers. Through thenga as a mercahnt you'll be able to 
+                post all the items you have available on stock -the sky is the limit- Thenga provides you with a modern dashbord allowing you 
+                to manage all your orders on the platform, with thenga you can directly communicate with your costomers agree on prices and more.
+                
+            </p>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            <p>
+                As a costomers you'll be able to support local merchats in your area , Discover incredible items right on your door step,
+                Thenga is your one stop shop
+            </p>
+
+        </div>
+    </container>)
+}
+ReactDOM.render(App(),document.getElementById("root"));
+// export App;
