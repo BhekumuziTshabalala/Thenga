c75e94196f4b455b32b47ea7cf23eeef95361992
Bhekumuzi Tshabalala <Bhekumuzitshabalala.main@gmail.com>
2024-02-07 10:30:44 +0200

    innitialised

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
-function App(){
-    return (<container>
-        <div>
-            <h1>Starting Page</h1>
-            <p>Thenga is a platform that connects local merchants to customers. Through thenga as a mercahnt you'll be able to 
-                post all the items you have available on stock -the sky is the limit- Thenga provides you with a modern dashbord allowing you 
-                to manage all your orders on the platform, with thenga you can directly communicate with your costomers agree on prices and more.
-                
-            </p>
 
-            <p>
-                As a costomers you'll be able to support local merchats in your area , Discover incredible items right on your door step,
-                Thenga is your one stop shop
-            </p>
+import  ReactDOM  from "react-dom";
 
-        </div>
-    </container>)
+const App = () => {
+   return (<main>
+        <h1>Hello World</h1>
+   </main> )
 }
-ReactDOM.render(App(),document.getElementById("root"));
-// export App;
+
+ReactDOM.render(App(),
+    document.getElementById("root"));
